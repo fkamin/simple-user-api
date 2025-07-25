20d5138bb694fd6880e2aee1f505235f14a4402c
Filip Kaminski <s95429@pollub.edu.pl>
2025-07-25 22:51:45 +0200

    Added InvalidLoginOrPasswordException

---
+++ src/main/java/home/simple_user_api/users/dtos/exceptions/InvalidLoginOrPasswordException.java
@@ -0,0 +1,11 @@
+package home.simple_user_api.users.dtos.exceptions;
+
+import home.simple_user_api.commons.ApiException;
+import home.simple_user_api.commons.ServiceErrorResponseCode;
+import org.springframework.http.HttpStatus;
+
+public class InvalidLoginOrPasswordException extends ApiException {
+    public InvalidLoginOrPasswordException() {
+        super(ServiceErrorResponseCode.AUTH01, HttpStatus.BAD_REQUEST);
+    }
+}
