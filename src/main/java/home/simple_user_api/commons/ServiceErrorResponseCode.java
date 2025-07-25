6a9c92f63e5f01b710cd66e1144080760e4e989e
Filip Kaminski <s95429@pollub.edu.pl>
2025-07-25 22:17:13 +0200

    Added user does not exists exception message

--- src/main/java/home/simple_user_api/commons/ServiceErrorResponseCode.java
+++ src/main/java/home/simple_user_api/commons/ServiceErrorResponseCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ServiceErrorResponseCode {
     ITEM01("You have not provided an authentication token, the one provided has expired, was revoked or is not authentic."),
-    ITEM02("You have not provided an authentication token, the one provided has expired, was revoked or is not authentic");
+
+    USER01("User does not exist exception");
 
     private final String message;
 
